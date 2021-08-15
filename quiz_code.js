737d071fde06dd7e1ae097538e93c2f511676f4d
swoopididoop <83615139+swoopididoop@users.noreply.github.com>
2021-08-15 20:45:55 +1200

    Studybuddy v14

--- quiz_code.js
+++ quiz_code.js
@@ -8,13 +8,14 @@ function userPressed1(){
 if(number==2){
     document.getElementById('question').textContent='What is your weakness in the subject you will study?';
     document.getElementById('qanswer_1').textContent='Application questions';
-    document.getElementById('qanswer_2')
-    .textContent='Definition and knowledge';}
+    document.getElementById('qanswer_2').textContent='Definition and knowledge';}
 else 
 if(number==3){
-    document.getElementById('question').textContent='Are you a visual learner?';
-    document.getElementById('qanswer_1').textContent='Yes';
-    document.getElementById('qanswer_2').textContent='No';
+    window.location.href= 'Past_papers_page.html';
+}
+else
+if(number==4){
+    window.location.href= 'Mindmapping_page.html';
 }
 }
 //when user selects answer 2
@@ -22,14 +23,17 @@ function userPressed2(){
     number=number+1;
 //changes question based on question number
 document.getElementById('qnumber').textContent='Question '+number ;
+//Pomodoro
 if(number==2){
-    document.getElementById('question').textContent='What is your weakness in the subject you will study?';
-    document.getElementById('qanswer_1').textContent='Application questions';
-    document.getElementById('qanswer_2').textContent='Definition and knowledge';}
+    window.location.href= 'Pomodoro_page.html';
+    }
 else 
 if(number==3){
     document.getElementById('question').textContent='Are you a visual learner?';
     document.getElementById('qanswer_1').textContent='Yes';
     document.getElementById('qanswer_2').textContent='No';
 }
-}
+else
+if(number==4){
+    window.location.href= 'Spaced_repetition_page.html';
+}}
